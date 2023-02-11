ff84af8f1b5b00d30b2b38370fe7d295eaf84d92
AnikinVladislav <vlad9anikin1999@gmail.com>
2023-02-11 23:10:16 +0300

    auth begining

--- fin_venv/web_finance/users/forms.py
+++ fin_venv/web_finance/users/forms.py
@@ -1,2 +1,25 @@
 # from django import forms
 # from .models import *
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class RegisterUserForm(UserCreationForm):
+    username = forms.CharField(label='Login', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    first_name = forms.CharField(label='First_name', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    last_name = forms.CharField(label='Last_name', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.CharField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-input'}))
+    
+    class Meta:
+        model = User
+        fields = ('username', 'password1', 'password2', 'first_name', 'last_name', 'email')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-input'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-input'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-input'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-input'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-input'}),
+            'email': forms.EmailInput(attrs={'class': 'form-input'}),
+        }
--- fin_venv/web_finance/users/views.py
+++ fin_venv/web_finance/users/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import *
 
 # Create your views here.
 def login(request):
     return render(request, "users/login.html")
+
+
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = "users/register.html"
+    success_url = "users/login.html"
