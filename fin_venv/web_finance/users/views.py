270e71799184707065ee45693cc6e4163fbfb391
AnikinVladislav <vlad9anikin1999@gmail.com>
2023-02-12 13:53:49 +0300

    add login and logout

--- fin_venv/web_finance/users/views.py
+++ fin_venv/web_finance/users/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
-# from django.contrib.auth.forms import UserCreationForm
+from django.views import generic
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
+from django.contrib import messages
+
 from .forms import *
 
 # Create your views here.
-def login(request):
-    return render(request, "users/login.html")
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/..')
+        else:
+            messages.success(request, ("Erorr Logging In, Try Again"))
+            return redirect('login')  
+    else:
+        return render(request, "users/login.html")
 
 
-class RegisterUser(CreateView):
-    form_class = RegisterUserForm
+class RegisterUser(generic.CreateView):
+    form_class = UserCreationForm
     template_name = "users/register.html"
-    success_url = "users/login.html"
+    success_url = reverse_lazy('login')
