8ffdf1088b01d3e06abc1118710b0b95041bd2f2
Anikin Vladislav <vlad9anikin1999@gmail.com>
2023-02-16 11:33:17 +0300

    add show spendings

--- fin_venv/web_finance/spendings/views.py
+++ fin_venv/web_finance/spendings/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
-def spendigs_view(request):
-    return render(request, "spendings/spendings_view.html")
+def spendigs_view(request, id):
+    myuser = User.objects.get(id=id)
+    myuserspendings = spendings.objects.filter(user=myuser).values()
+    allcategories = categories.objects.all().values()
+    context = {
+        'myuser': myuser,
+        'myuserspendings': myuserspendings,
+        'allcategories': allcategories
+    }
+    return render(request,'spendings/list_spendings.html',  context)
+
+
